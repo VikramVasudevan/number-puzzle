66b6f8a76e82bb9abb4a5f323ba07836b0e1a46b
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2022-11-25 17:57:31 +0530

    grid is now configurable

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { NumberGrid, NumberGridTest } from './NumberGrid/NumberGrid';
-
-function uniqueRandomNumbers(max, qty) {
-  const retVal = new Set();
-  console.log("retVal.size = ", retVal.size);
-  while (retVal.size < qty) {
-    retVal.add(Math.floor(Math.random() * max));
-  }
-  const arr = Array.from(retVal);
-  let assignments = [];
-  for (var row = 0; row < initialGrid.numRows; row++) {
-    assignments.push([]);
-    for (var col = 0; col < initialGrid.numCols; col++) {
-      var obj = {
-        row: row,
-        col: col,
-        value: arr[initialGrid.numCols * row + (col)]
-      };
-      assignments[row].push(obj);
-    }
-  }
-  console.log("assignments = ", assignments);
-  return assignments;
-}
+import { NumberGrid, NumberGridTest, uniqueRandomNumbers } from './NumberGrid/NumberGrid';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 let initialGrid = {
+  maxNumber: 100,
   numRows: 4,
   numCols: 4,
   assignments: undefined
 };
 
-initialGrid.assignments = uniqueRandomNumbers(100, initialGrid.numRows * initialGrid.numCols - 1);
+initialGrid.assignments = uniqueRandomNumbers(initialGrid.maxNumber, initialGrid.numRows * initialGrid.numCols - 1, { numRows: initialGrid.numRows, numCols: initialGrid.numCols });
 
 console.log("grid = ", initialGrid);
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <table>
-          <tbody>
-            <NumberGrid initialGrid={initialGrid}></NumberGrid>
-          </tbody>
-        </table>
+        <Container fluid>
+          <Row>
+            <Col><img src={logo} className="App-logo" alt="logo" /></Col>
+            <Col><NumberGrid initialGrid={initialGrid}></NumberGrid></Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
--- src/NumberGrid/NumberGrid.js
+++ src/NumberGrid/NumberGrid.js
@@ -1,5 +1,33 @@
 import Button from 'react-bootstrap/Button';
 import { useEffect, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export function uniqueRandomNumbers(max, qty, gridDimensions) {
+    const retVal = new Set();
+    console.log("retVal.size = ", retVal.size);
+    while (retVal.size < qty) {
+        retVal.add(Math.floor(Math.random() * max));
+    }
+    const arr = Array.from(retVal);
+    let assignments = [];
+    for (var row = 0; row < gridDimensions.numRows; row++) {
+        assignments.push([]);
+        for (var col = 0; col < gridDimensions.numCols; col++) {
+            var obj = {
+                row: row,
+                col: col,
+                value: arr[gridDimensions.numCols * row + (col)]
+            };
+            assignments[row].push(obj);
+        }
+    }
+    console.log("assignments = ", assignments);
+    return assignments;
+}
 
 function moveButton(row, col, grid, setGrid) {
     console.debug('Moving button ', row, col, grid.assignments[row][col])
@@ -54,8 +82,8 @@ function getButton(grid, setGrid, row, col) {
 
     try {
         buttonObj = grid.assignments[row][col];
-        if (buttonObj.value)
-            return <Button onClick={() => moveButton(row, col, grid, setGrid)}>{row},{col} =  {buttonObj.value}</Button>
+        if (typeof buttonObj.value != 'undefined')
+            return <Button onClick={() => moveButton(row, col, grid, setGrid)}>{buttonObj.value}</Button>
     } catch (e) {
         console.error(e);
     }
@@ -72,6 +100,7 @@ function renderColumns(grid, setGrid, rowNumber) {
 }
 
 function renderGrid(grid, setGrid) {
+    console.log("Rendering grid")
     return Array(grid.numRows).fill().map(function (v, i) {
         return <tr>
             {renderColumns(grid, setGrid, i)}
@@ -105,23 +134,92 @@ export function NumberGridTest(props) {
     );
 }
 
+function getNumberOfMovesAlert(movesCount) {
+    if (movesCount > 20) {
+        return (<Alert variant="danger">
+            Number of moves - {movesCount}
+        </Alert>);
+    }
+    else if (movesCount > 10) {
+        return (<Alert variant="warning">
+            Number of moves - {movesCount}
+        </Alert>);
+    }
+    else
+        return (<Alert variant="success">
+            Number of moves - {movesCount}
+        </Alert>);
+}
 
 export function NumberGrid(props) {
     const [grid, setGrid] = useState(props.initialGrid);
+    const [movesCount, setMovesCount] = useState(0);
 
     const setGridState = (newGrid) => {
-        console.log("Setting new grid state", newGrid);
+        console.log("Setting new grid state", newGrid, movesCount);
+        setGrid(newGrid);
+        setMovesCount(movesCount + 1);
+    }
+
+    const changeGridSize = (changeType, newVal) => {
+        let newGrid = JSON.parse(JSON.stringify(grid));
+        newVal = parseInt(newVal);
+        if (changeType === 'rows')
+            newGrid.numRows = newVal;
+        else if (changeType === 'cols')
+            newGrid.numCols = newVal;
+
+        newGrid.assignments = uniqueRandomNumbers(newGrid.maxNumber, newGrid.numRows * newGrid.numCols - 1, { numRows: newGrid.numRows, numCols: newGrid.numCols });
+        setMovesCount(0);
         setGrid(newGrid);
     }
 
     useEffect(() => {
-        console.log("State updated ...", grid);
+        console.log("Grid updated ...", grid);
     }, [grid])
+
+    useEffect(() => {
+        console.log("movesCount updated ...", movesCount);
+    }, [movesCount])
 
     return (
         <div>
+            <Form.Group className="mb-3" controlId="formGridRows" onChange={e => changeGridSize('rows', e.target.value)}>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <Form.Text className="text-muted">
+                                Grid Rows
+                            </Form.Text>
+                        </Col>
+                        <Col><Form.Control type="number" placeholder="Grid Rows" value={grid.numRows} /></Col>
+                    </Row>
+                </Container>
+
+
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridColumns" onChange={e => changeGridSize('cols', e.target.value)}>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <Form.Text className="text-muted">
+                                Grid Columns
+                            </Form.Text>
+                        </Col>
+                        <Col><Form.Control type="number" placeholder="Grid Rows" value={grid.numCols} /></Col>
+                    </Row>
+                </Container>
+            </Form.Group>
+
+            <h1>{grid.numRows} * {grid.numCols} Grid</h1>
+            {getNumberOfMovesAlert(movesCount)}
             <div>
-                { renderGrid(grid, setGridState) }
+                <table>
+                    <tbody>
+                        {renderGrid(grid, setGridState)}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
